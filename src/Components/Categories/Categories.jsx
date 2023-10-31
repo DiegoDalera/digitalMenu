3e1949e5e702b69905a703bc286fb40829821d52
DiegoDalera <dmdalera@gmail.com>
2023-10-31 17:13:41 -0300

    categorie filter

--- src/Components/Categories/Categories.jsx
+++ src/Components/Categories/Categories.jsx
@@ -1,15 +1,19 @@
 
-import products from '../../Data/products.json'; 
+import products from '../../Data/products.json';
 import "./Categories.css";
 
-function Categories() {
-  // Extrae las categorías únicas usando un conjunto (Set)
-  const uniqueCategories = [...new Set(products.map(product => product.category))];
-
+function Categories({ onCategorySelect }) {
+  
+  const uniqueCategories = ["Todas", ...new Set(products.map(product => product.category))];
+  
   return (
     <div className="categories">
       {uniqueCategories.map(category => (
-        <div key={category} className="category-item">
+        <div 
+          key={category} 
+          className="category-item"
+          onClick={() => onCategorySelect(category === "Todas" ? null : category)}
+        >
           {category}
         </div>
       ))}
