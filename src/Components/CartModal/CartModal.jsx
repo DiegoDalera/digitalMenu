3f5a40eb45c92e2fbcc42876193d08d8314f8440
DiegoDalera <dmdalera@gmail.com>
2023-11-02 11:02:54 -0300

    consultas separadas

--- src/Components/CartModal/CartModal.jsx
+++ src/Components/CartModal/CartModal.jsx
@@ -1,15 +1,83 @@
 /* eslint-disable react/prop-types */
+// import "./CartModal.css";
+// import products from "../../Data/products.json";
+
+// function CartModal({ onClose }) {
+//   // Obtener la lista de IDs del pedido del localStorage
+//   const orderIds = JSON.parse(localStorage.getItem("order")) || [];
+
+//   // Encontrar los productos solicitados por ID
+//   const orderedProducts = products.filter((product) =>
+//     orderIds.includes(product.id)
+//   );
+
+//   return (
+//     <div className="cart-modal">
+//       <button onClick={onClose}>Cerrar</button>
+//       <h3 className="order-title">Tu Pedido</h3>
+//       {orderedProducts.length > 0 ? (
+//         <>
+//           <ul className="product-list">
+//             {orderedProducts.map((product) => (
+//               <li className="product" key={product.id}>
+//                 <div className="product-image">
+//                   <img
+//                     src={product.image}
+//                     alt={product.title}
+//                     className="product-img"
+//                   />
+//                 </div>
+//                 <div className="product-text">
+//                   <h4 className="product-title">{product.title}</h4>
+//                   <p className="product-description">{product.descripcion}</p>
+//                   <p className="product-prize">
+//                     Precio: ${product.prize.toFixed(2)}
+//                   </p>
+//                 </div>
+//               </li>
+//             ))}
+//           </ul>
+//           <button className="checkout-button">Proceder al Pago</button>
+//         </>
+//       ) : (
+//         <p>Tu carrito está vacío.</p>
+//       )}
+//     </div>
+//   );
+// } 
+
+// export default CartModal;
+
+import { useState, useEffect } from 'react';
+import { getFirestore, collection, query, where, getDocs } from 'firebase/firestore';
+import app from '../../Data/firebaseApp'; 
 import "./CartModal.css";
-import products from "../../Data/products.json";
 
 function CartModal({ onClose }) {
-  // Obtener la lista de IDs del pedido del localStorage
-  const orderIds = JSON.parse(localStorage.getItem("order")) || [];
 
-  // Encontrar los productos solicitados por ID
-  const orderedProducts = products.filter((product) =>
-    orderIds.includes(product.id)
-  );
+  const [orderedProducts, setOrderedProducts] = useState([]);
+
+  useEffect(() => {
+    alert("entro")
+    const fetchOrderedProducts = async () => {
+      
+      const db = getFirestore(app);
+      const orderIds = JSON.parse(localStorage.getItem("order")) || [];
+      
+      
+      if (orderIds.length > 0) {
+        const productsCol = collection(db, 'menu');
+        const queryRef = query(productsCol, where('id', 'in', orderIds));
+        const querySnapshot = await getDocs(queryRef);
+        // Actualizar el estado con los productos obtenidos
+        const fetchedProducts = querySnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+        alert(fetchedProducts)
+        setOrderedProducts(fetchedProducts);
+      }
+    };
+
+    fetchOrderedProducts();
+  }, []);
 
   return (
     <div className="cart-modal">
@@ -29,7 +97,7 @@ function CartModal({ onClose }) {
                 </div>
                 <div className="product-text">
                   <h4 className="product-title">{product.title}</h4>
-                  <p className="product-description">{product.descripcion}</p>
+                  <p className="product-description">{product.descripcion}</p> {/* Asegúrate de que la propiedad es 'description' y no 'descripcion' en tu base de datos */}
                   <p className="product-prize">
                     Precio: ${product.prize.toFixed(2)}
                   </p>
@@ -47,4 +115,3 @@ function CartModal({ onClose }) {
 } 
 
 export default CartModal;
-
--- src/Components/ItemList/ItemList.jsx
+++ src/Components/ItemList/ItemList.jsx
@@ -1,34 +1,84 @@
 /* eslint-disable react/prop-types */
 
-import products from '../../Data/products.json';
+// import products from '../../Data/products.json';
+// import Item from '../Item/Item'
+// import './ItemList.css';
+
+
+// function ItemList({ category,onAddToOrder })  {
+
+//   const filteredProducts = category 
+//     ? products.filter(product => product.category === category)
+//     : products;
+
+//     const handleAddToOrder = (productId) => {
+      
+//       const currentOrder = JSON.parse(localStorage.getItem('order')) || [];
+//       currentOrder.push(productId);
+//       localStorage.setItem('order', JSON.stringify(currentOrder));
+
+//       onAddToOrder();
+//   }
+  
+
+//   return (
+//     <div className="item-list">
+//       {filteredProducts.map(product => (
+//         <Item key={product.id}
+//               id={product.id} 
+//               image={product.image} 
+//               title={product.title} 
+//               descripcion={product.descripcion} 
+//               prize={product.prize}
+//               onAddToOrder={handleAddToOrder} />
+//       ))}
+//     </div>
+//   );
+// }
+
+// export default ItemList;
+
+import { useState, useEffect } from 'react';
+import { getFirestore, collection, query, where, getDocs } from 'firebase/firestore';
+import app from '../../Data/firebaseApp'
 import Item from '../Item/Item'
 import './ItemList.css';
 
+function ItemList({ category, onAddToOrder }) {
 
-function ItemList({ category,onAddToOrder   })  {
+  const [products, setProducts] = useState([]);
 
-  const filteredProducts = category 
-    ? products.filter(product => product.category === category)
-    : products;
+  useEffect(() => {
+    
+    const db = getFirestore(app);
+    const productsCol = collection(db, 'menu');
+    const queryRef = category ? query(productsCol, where('category', '==', category)) : productsCol;
 
-    const handleAddToOrder = (productId) => {
-      
-      const currentOrder = JSON.parse(localStorage.getItem('order')) || [];
-      currentOrder.push(productId);
-      localStorage.setItem('order', JSON.stringify(currentOrder));
+    
+    const fetchProducts = async () => {
+      const querySnapshot = await getDocs(queryRef);
+      const productList = querySnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+      setProducts(productList);
+    };
 
-      onAddToOrder();
+    fetchProducts();
+  }, [category]); // Este efecto se ejecutará de nuevo cuando la categoría cambie
+
+  const handleAddToOrder = (productId) => {
+    const currentOrder = JSON.parse(localStorage.getItem('order')) || [];
+    currentOrder.push(productId);
+    localStorage.setItem('order', JSON.stringify(currentOrder));
+    onAddToOrder();
   }
-  
 
   return (
     <div className="item-list">
-      {filteredProducts.map(product => (
+      {products.map(product => (
         <Item key={product.id}
-              id={product.id} 
-              image={product.image} 
-              title={product.title} 
-              descripcion={product.descripcion} 
+              id={product.id}
+              image={product.image}
+              title={product.title}
+              descripcion={product.descripcion}
               prize={product.prize}
               onAddToOrder={handleAddToOrder} />
       ))}
